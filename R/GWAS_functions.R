e2ce5ec07e9d180ef40cf5fb0f69178b6d840e1d
Niklas Schandry <niklas.schandry@gmi.oeaw.ac.at>
2019-10-28 11:41:47 +0000

    more facet fixing

--- R/GWAS_functions.R
+++ R/GWAS_functions.R
@@ -225,7 +225,7 @@ plot_intersect_phenotype_snp <- function(phenotype_table, phenotype, gwas_table,
          x = "SNP present",
          y = "Value") +
     theme_bw() +
-    facet_grid(vars(Phenotype), vars(eval(specific)))
+    facet_grid(eval(expr(!!ensym(Phenotype) ~ !!ensym(specific)))) #????
   print(p)
 }
 
